8552a62901310dc0822f6d5012cb5911535bf324
cyuzuzo <cyuzuzo.jambe@gmail.com>
2023-02-24 13:25:52 +0100

    commit2

--- hz2/src/hz2/TimeOfDay.java
+++ hz2/src/hz2/TimeOfDay.java
@@ -1,9 +1,12 @@
 package hz2;
-
+/**
+* @invar | (0 <= getHour()&&  getHour() <= 23)
+* @invar | (0 <= getMinutes() && getMinutes() <= 59)
+* */
 public class TimeOfDay {
 		/**
 		 * @invar | (0 <= uur && uur <= 23)
-		 * @invar | (0 <= minuten && minuten <= 23)
+		 * @invar | (0 <= minuten && minuten <= 59)
 		 */
 		private int uur;
 		private int minuten;
@@ -25,25 +28,25 @@ public class TimeOfDay {
 			this.uur = uur;
 		}
 		/**
-		 * @throws | !(0 <= minuten && minuten <= 23)
+		 * @throws | !(0 <= minuten && minuten <= 59)
 		 * @post   | this.getMinutes() == minuten
 		 */
 		void setMinutes(int minuten){
-			if(!(0 <= minuten && minuten <= 23)) {
-				throw new IllegalArgumentException("0<=minuten <= 23");}
+			if(!(0 <= minuten && minuten <= 60)) {
+				throw new IllegalArgumentException("0<=minuten <= 59");}
 			this.minuten = minuten;
 		}
 		//constructor
 		/**
 		 * @throws | !(0 <= uur && uur <= 23)
-		 * @throws | !(0 <= minuten && minuten <= 23)
+		 * @throws | !(0 <= minuten && minuten <= 59)
 		 * @post   | this.getHour() == uur && this.getMinutes() == minuten
 		 */
 		TimeOfDay(int uur,int minuten){
 			if(!(0 <= uur && uur <= 23)) {
 					throw new IllegalArgumentException("0<=uur <= 23");}
-			if(!(0 <= minuten && minuten <= 23)) {
-				throw new IllegalArgumentException("0<=minuten <= 23");}
+			if(!(0 <= minuten && minuten <= 59)) {
+				throw new IllegalArgumentException("0<=minuten <= 59");}
 			this.uur = uur;
 			this.minuten = minuten;
 		}
